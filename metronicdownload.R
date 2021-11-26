749f306b5a71a7c101c83b01bb4541f1cace3320
Luis Barrios <20141112@lamolina.edu.pe>
2021-11-25 21:40:33 -0500

    323
    3

--- metronicdownload.R
+++ metronicdownload.R
@@ -2,13 +2,24 @@ library(RSelenium)
 library(rvest)
 library(tidyverse)
 library(lubridate)
+library(readxl)
+library(lubridate)
+Sys.sleep(10)
+# este pagina es la que nos brinda la parte de application/vnd.... , esto es necesario ponerlo para que el buscador de firefox no nos pregunte si queremos 
+# guardar o abrir el archivo, porque eso mata todo el codigo, mediante este codigo, especifico para documentos con terminación xls, podemos saltar el pop-up
+# y descargar directamente en la carpeta de descargas o downloads de la página.
+# https://www.freeformatter.com/mime-types-list.html
+driver <- rsDriver(browser = c("firefox"),port = 4440L, extraCapabilities = makeFirefoxProfile(list(
+  "browser.helperApps.neverAsk.saveToDisk"="application/vnd.ms-excel")
+))
 
-driver <- rsDriver(browser = c("firefox"),port = 4447L)
 remote_driver <- driver[["client"]]
 
+# install.packages("Rtools") por seacaso
 
+Sys.sleep(6)
 remote_driver$navigate("http://www.lansierweb.com:9300/FACTURACION/FacturacionMetronic.aspx?JER=divMenuLBTR_1_01.02")
-
+Sys.sleep(7)
 # con esto hacemos click en LABORATORIOS LANSIER S.A.C 
 # FORMA GENIAL DE SELECCIONAR MEDIANTE ID Y VALUE, NO SABIA HACERLA ANTES
 empresa<- remote_driver$findElement(using = 'xpath', "//select[@id='ddlEmpresa']/option[@value='1']")
@@ -36,7 +47,7 @@ psswd$sendKeysToElement(list("72722698","\uE007"))
 # ahora que ya entre a la pagina solo falta hacer click en  descargar excel
 # DESCARGAR EXCEL
 # PARA SELECCIONAR EL EXCEL AL PARECER CUALQUIERA DE ESTOS COMANDOS ME ES VALIDO, MAS ADELANTE PROBAR DE NUEVO
-
+Sys.sleep(7)
 downloadexcel<-remote_driver$findElement(using = "id",value = "ctl00_ContentPlaceHolder1_ibtn_Excel")
 # downloadexcel<-remote_driver$findElement(using = "class",value = "Boton")
 # downloadexcel<-remote_driver$findElement(using = 'xpath', "//select[@id='ddlEmpresa']/option[@value='1']")
@@ -48,45 +59,34 @@ downloadexcel<-remote_driver$findElement(using = "id",value = "ctl00_ContentPlac
 # downloadexcel<-remote_driver$findElement(using = 'xpath', "//*[@id='ctl00_ContentPlaceHolder1_ibtn_Excel']")
 # downloadexcel$getElementLocation()
 # downloadexcel<-remote_driver$mouseMoveToLocation()
-
+Sys.sleep(2)
 # CLICK DESCARGA EXCEL: ESTAMOS USANDO OTRO METODO DISTINTO AL CLICK ELEMENT ANTERIOR, AHORA ESTAMOS HACIENDO OTRO METODO, SITUANDO EL MOUSE EN LA COORDENADA Y DANDOLE CLICK
 remote_driver$mouseMoveToLocation(webElement = downloadexcel)
 remote_driver$click(buttonId = "RIGHT")
 
 # CON ESTO HASTA AQUI YA DEBERIA ESTAR DESCARGADO EL EXCEL
-#########################################################################################
 
+############################################################################################
+############################################################################################
+############    TRABAJANDO LA DATA    ############    TRABAJANDO LA DATA    ################
+############################################################################################
+############################################################################################
 
+# gracias a este paquete pude leer el excel, ya que de otra forma no podia
+# install.packages("XLConnect")
+library(XLConnect)
+library(dplyr)
+Sys.sleep(3)
+data <- readWorksheetFromFile("C:\\Users\\LBarrios\\Downloads\\FacMet_20211125.xls", sheet = "FacturacionMetronic",
+                              startRow = 6,
+                              startCol = 1)
 
+# retirando las columnas que eran merge de las otras
+data<-data %>% select(-Col6,-Col12)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+data2<-data %>% filter(Periodo=="202111")
+data3<-data2 %>% filter(Artículo!="MACUVIT X 60 CAPSULAS")
+rm(data3)
 
 
 
